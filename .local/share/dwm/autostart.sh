84f3b20e289f14edf442f7eb9177765f87975686
rLapz <arthurlapz@gmail.com>
2021-05-19 03:11:19 +0700

    add new config files

---
+++ .local/share/dwm/autostart.sh
@@ -0,0 +1,24 @@
+dwmblocks &
+pipewire &
+pipewire-pulse &
+pipewire-media-session &
+#randomfeh 30 ~/Pictures/Wallpapers &
+#feh --bg-scale ~/Pictures/Wallpapers/1085235.png
+#feh --bg-fill ~/Pictures/wallpaper.jpg
+sxhkd &
+clipmenud &
+dunst &
+#xcompmgr -c -C -t-5 -l-5 -r4.2 -o.55 &
+#picom &
+#nm-applet &
+#/usr/bin/viper start &
+#crow &
+redshift &
+/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+#caffeine &
+#xpad &
+#osmo &
+#sleep 5 &&
+#librespot -n "rLapz-dev" -b 320 -c ~/.cache --enable-volume-normalisatio &
+#sleep 2 && volumeicon &
+sleep 2 && telegram-desktop -autostart &
---
+++ .vim/after/ftplugin/py.vim
@@ -0,0 +1,7 @@
+set formatoptions=tcqrn1
+set autoindent
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+set noshiftround
